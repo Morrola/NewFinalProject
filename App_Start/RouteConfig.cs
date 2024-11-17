2bdb5e9a98d14cc31fa844224422aff8cc5ad70c
Morrola <143765846+Morrola@users.noreply.github.com>
2024-11-17 23:49:45 +0300

    Add files via upload

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Web
+{
+    public class RouteConfig
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional },
+                new string[] {"CoursesApp.Controllers"}
+
+         
+
+            );
+
+        }
+    }
+}
