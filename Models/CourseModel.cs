2eda800de450ce358408fb08f70f4259884557ff
Morrola <143765846+Morrola@users.noreply.github.com>
2024-11-17 23:51:27 +0300

    Add files via upload

---
+++ Models/CourseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Models
+
+{
+
+    public class CourseModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public CategoryModel Category { get; set; }
+
+    }
+}
